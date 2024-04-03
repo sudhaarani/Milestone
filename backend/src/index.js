894aa543200507f4a7ef0a265a80a64e406d0873
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-02 18:25:42 -0700

    db setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
-const express  = require("express"); // Import the express library
+// load .env data into process.env
+require('dotenv').config();
+
+const express = require("express"); // Import the express library
 const app = express(); // Define our app as an instance of express
-const port = 8000; // Define our base URL as http:\\localhost:3000
+const port = process.env.DB_PORT ; 
 
 app.listen(port, function () {
-  console.log(`Server running on port ${port}`); // Tell yourself the port number to prevent mistakes in the future.
+  console.log(`Server running on port ${port}`);
 });
