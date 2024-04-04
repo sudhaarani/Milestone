f7043c201e2a5dd033444ad7566b2d4c1fa06e91
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-04 12:51:22 -0700

    add import and use lines for cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,15 @@ require('dotenv').config();
 const express = require("express"); 
 const app = express();
 const morgan = require('morgan');
+const cors = require("cors");
 const port = process.env.PORT || 8001; 
 
 //session-cookie is needed
 
  app.use(morgan('dev')); //to log HTTP requests to your application's console
  app.use(express.urlencoded({ extended: true })); //parses the data and makes it available in the req.body object
-
+ app.use(cors());
+ 
  const usersRoutes = require('./routes/users');
  const milestonesRoutes = require('./routes/milestones');
  const timelinesRoutes = require('./routes/timelines');
