2d553cd3d2567a382e77eddf3297fb16a56c98b2
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-06 00:19:10 -0700

    add HomePage,TimelineEditModal components and import the state and functions from useApplicationData custom hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import React from 'react';
+
+import HomePage from './components/HomePage';
+import TimelineEditModal from './components/TimelineEditModal';
+
+import useApplicationData from "../src/hooks/useApplicationData";
+
 
 function App() {
+  const {
+    state,setDisplayModal,handleModalTimeline
+  } = useApplicationData();
   return (
     <div className="App">
+      <HomePage state={state} setDisplayModal={setDisplayModal} handleModalTimeline={handleModalTimeline} />
+      {state.displayModal && <TimelineEditModal setDisplayModal={setDisplayModal} state={state}
+       />}
     </div>
   );
 }
