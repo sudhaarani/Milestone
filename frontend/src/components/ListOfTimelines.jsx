ea8da5492ed4442b130b774cf43022849448298d
Neeharika Baral <neehabaral72@gmail.com>
2024-04-08 00:42:39 -0400

    add bootstrap and styling for ListOfTimelines and TimelineCard

--- frontend/src/components/ListOfTimelines.jsx
+++ frontend/src/components/ListOfTimelines.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-
 import TimelineCard from "./TimelineCard";
+import '../styles/ListOfTimelines.css'
 
 const ListOfTimelines = ({state, handleToggle, handleSelectedTimeline}) => {
+  console.log(state.timelines)
 
   const TimelineListArray = state.timelines.map((timelineList) => {
-    return (<TimelineCard 
-            key={timelineList.id}
-            timelineList={timelineList}
-            handleToggle={handleToggle}
-            handleSelectedTimeline={handleSelectedTimeline}
-            state={state}
-            />)
+    return (<div className="timelines-card-list">
+              <TimelineCard 
+                key={timelineList.id}
+                timelineList={timelineList}
+                handleToggle={handleToggle}
+                handleSelectedTimeline={handleSelectedTimeline}
+                state={state}
+                />
+            </div>)
   });
 
   return (
--- frontend/src/components/TimelineCard.jsx
+++ frontend/src/components/TimelineCard.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import '../styles/TimelineCard.css'
 
 const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, state}) => {
-  const { id, title, description } = timelineList;
+  const { id, title, description, timelineImageUrl } = timelineList;
 
   return (
-    <div>
-        <p>{title}</p>
-        <p>{description}</p>
-
-        <button type="submit" onClick={() => { 
-            handleToggle() 
-            handleSelectedTimeline(id)
-          }}> Edit </button>
-        <button>Delete</button>
-
+    <div className="card">
+      <img src={timelineImageUrl} className="card-img-top" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <button type="button" className="btn btn-primary" onClick={() => { 
+          handleToggle() 
+          handleSelectedTimeline(id)
+        }}> Edit </button>
+        <button type="button" className="btn btn-danger">Delete</button>
+      </div>
     </div>
   );
 }
