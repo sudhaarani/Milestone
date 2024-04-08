f42f6cc067d3c204db8d2f5c8cc888872fc48368
Neeharika Baral <neehabaral72@gmail.com>
2024-04-08 00:39:21 -0400

    organize backend index.js and timelines.js routes file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,47 @@
-// load .env data into process.env
-require('dotenv').config();
+////////////////////////////////////////////////////////
+//            basic server configuration              //
+////////////////////////////////////////////////////////
 
-const express = require("express"); 
+require('dotenv').config();
+const express = require('express'); 
 const app = express();
-const morgan = require('morgan');
-const cors = require("cors");
 const port = process.env.PORT || 8001; 
 
-//session-cookie is needed
 
- app.use(morgan('dev')); //to log HTTP requests to your application's console
- app.use(express.urlencoded({ extended: true })); //parses the data and makes it available in the req.body object
- app.use(cors());
- 
- const usersRoutes = require('./routes/users');
- const milestonesRoutes = require('./routes/milestones');
- const timelinesRoutes = require('./routes/timelines');
 
+////////////////////////////////////////////////////////
+//                     middleware                     //
+////////////////////////////////////////////////////////
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+  // to log HTTP requests to your app's console
+
+const path = require('path'); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+  // to serve static files from public directory 
+
+app.use(express.urlencoded({ extended: true }));
+  // to parse data and make available in req.body
+
+
+
+////////////////////////////////////////////////////////
+//                      routing                       //
+////////////////////////////////////////////////////////
+
+const usersRoutes = require('./routes/users');
+const milestonesRoutes = require('./routes/milestones');
+const timelinesRoutes = require('./routes/timelines');
 app.use('/api', usersRoutes);
 app.use('/api', milestonesRoutes);
 app.use('/api', timelinesRoutes);
+
+
+
+////////////////////////////////////////////////////////
+//                  server listner                    //
+////////////////////////////////////////////////////////
 
 app.listen(port, function (error) {
   if (error) {
--- backend/src/routes/timelines.js
+++ backend/src/routes/timelines.js
@@ -5,13 +5,20 @@ router.use(express.json());
 
 const db = require("../db/connection.js");
 
-router.get("/timelines", (request, response) => {  
-  db.query(`SELECT * FROM timelines;`).then(({ rows: users }) => {
-    response.json(users);
-  }).catch(err => {
-    console.error(err);
-    response.status(500).send('Server Error');
-  });
+router.get('/timelines', (req, res) => {  
+  db.query(`SELECT * FROM timelines;`)
+    .then(({ rows: timelines }) => {
+      const updatedTimelinesObj = timelines.map(timeline => (
+        { ...timeline,
+          timelineImageUrl: `/uploads/${timeline.image}`
+        }
+      ));
+      res.json(updatedTimelinesObj);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Server Error');
+    });
 });
 
 
