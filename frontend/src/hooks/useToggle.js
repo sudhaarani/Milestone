569be735c890e1bacc866a58437d05626142022b
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-08 17:47:22 -0700

    add another state called favoritesState and method called handleFavorite to keep track of favorited ids

--- frontend/src/hooks/useToggle.js
+++ frontend/src/hooks/useToggle.js
@@ -2,12 +2,28 @@ import { useState } from "react";
 
 const useToggle = (initialState = false) => {
   const [toggleState, setToggleState] = useState(initialState);
+  const [favoritesState, setFavoritesState] = useState([]);
+
 
   const handleToggle = () => {
     setToggleState(!toggleState);
   };
 
-  return { toggleState, handleToggle };
+  const handleFavorite = (id) => {
+    //console.log("handleFavorite :: id:", id);
+    setFavoritesState(favPhotos => {
+      //console.log(favPhotos.includes(id));
+      if (favPhotos.includes(id)) {
+        // If yes, remove it from the favorite list
+        return favPhotos.filter(_id => _id !== id);
+      } else {
+        // If not, add it to the favorite list
+        return [...favPhotos, id];
+      }
+    })
+  }
+
+  return { toggleState,favoritesState, handleToggle, handleFavorite};
 }
 
 export default useToggle;
