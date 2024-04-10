da897f4ba8062e63dee580d37956a590bb915525
Rico Chow <rico.ck.chow@gmail.com>
2024-04-10 13:10:03 -0400

    Use react-router to update NavBar.jsx and App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,38 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
 import HomePage from './components/HomePage';
 import MainModal from './components/MainModal';
 import NavBar from './components/NavBar';
 import NewTimelineForm from './components/NewTimelineForm';
 
-import useApplicationData from "../src/hooks/useApplicationData";
-import useToggle from "../src/hooks/useToggle";
-
+import useApplicationData from "./hooks/useApplicationData"; // Adjusted the path for hooks
+import useToggle from "./hooks/useToggle";
 
 function App() {
   const { state, handleSelectedTimeline, handleFavourites } = useApplicationData();
   const { toggleState, handleToggle} = useToggle();
 
   return (
-    <div className="App">
-      <NavBar isLoggedIn={true} username={"Labber"} />
+    <Router> {/* Use Router to wrap the application */}
+      <div className="App">
+        <NavBar isLoggedIn={true} username={"Labber"} />
 
-      <HomePage state={state} handleToggle={handleToggle} handleSelectedTimeline={handleSelectedTimeline}
-        handleFavourites={handleFavourites} />
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path="/" element={<HomePage 
+            state={state} 
+            handleToggle={handleToggle} 
+            handleSelectedTimeline={handleSelectedTimeline}
+            handleFavourites={handleFavourites} 
+          />} />
+          <Route path="/new-timeline" element={<NewTimelineForm />} />
+          {/* You can add more routes here for other components */}
+        </Routes>
 
-      {toggleState && <MainModal handleToggle={handleToggle} state={state} />}
-
-      <NewTimelineForm />
-    </div>
+        {toggleState && <MainModal handleToggle={handleToggle} state={state} />}
+        {/* Consider if MainModal should be part of a route or conditional rendering */}
+      </div>
+    </Router>
   );
 }
 
