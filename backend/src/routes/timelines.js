360e5fa2ab79ba335f964715cd9606802a321ec5
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-09 19:03:22 -0700

    add get route for checkpoint /timelines/milestones/:id to fetch milestones based on timeline id

--- backend/src/routes/timelines.js
+++ backend/src/routes/timelines.js
@@ -71,5 +71,20 @@ router.get('/timelines', (req, res) => {
     });
 });
 
+router.get('/timelines/milestones/:id', (req, res) => {  
+  db.query(`SELECT timelines.*,milestones.id as milestone_id,milestones.title as milestone_title, milestones.date as milestone_date,
+  image1,image2,image3,image4 
+  FROM timelines JOIN milestones ON milestones.timeline_id = timelines.id
+  WHERE timelines.id = ${req.params.id};`)
+    .then(({ rows: users }) => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Server Error');
+    });
+});
+// 
+
 
 module.exports = router;
