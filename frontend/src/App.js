3756107060c5d00943cb30d01e86b57190fe42ff
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-09 19:05:57 -0700

    add TimelineViewModal component and get required function from useApplicationData hooks and send it as props to this component , comment MainModal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import HomePage from './components/HomePage';
 import MainModal from './components/MainModal';
+import TimelineViewModal from './components/TimelineViewModal';
 import NavBar from './components/NavBar';
 import NewTimelineForm from './components/NewTimelineForm';
 
@@ -10,7 +11,7 @@ import useToggle from "../src/hooks/useToggle";
 
 
 function App() {
-  const { state, handleSelectedTimeline, handleFavourites } = useApplicationData();
+  const { state, handleSelectedTimeline, handleFavourites,getMilestonesByTimeline } = useApplicationData();
   const { toggleState, handleToggle} = useToggle();
 
   return (
@@ -18,10 +19,10 @@ function App() {
       <NavBar isLoggedIn={true} username={"Labber"} />
 
       <HomePage state={state} handleToggle={handleToggle} handleSelectedTimeline={handleSelectedTimeline}
-        handleFavourites={handleFavourites} />
+        handleFavourites={handleFavourites} getMilestonesByTimeline={getMilestonesByTimeline} />
 
-      {toggleState && <MainModal handleToggle={handleToggle} state={state} />}
-
+      {toggleState && <TimelineViewModal handleToggle={handleToggle} state={state} />}
+      {/* {toggleState && <MainModal handleToggle={handleToggle} state={state} />} */}
       <NewTimelineForm />
     </div>
   );
