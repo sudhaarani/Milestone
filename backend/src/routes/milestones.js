7f9fd8287ed0fb0e2f1c31cf5ec4d97e63ae5d02
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-10 11:03:16 -0700

    change the checkpoint to have timeline_id and add it to where clause

--- backend/src/routes/milestones.js
+++ backend/src/routes/milestones.js
@@ -25,4 +25,21 @@ router.get('/milestones', (req, res) => {
 });
 
 
+router.get('/milestones/search/:timeline_id/:searchText', (req, res) => {  
+  let searchText = req.params.searchText;
+  let timeline_id = req.params.timeline_id;
+  console.log("searchText:",searchText)
+  searchText=searchText.toLowerCase();
+  db.query(`SELECT id as milestone_id,timeline_id, title as milestone_title, date as milestone_date, image1,image2,image3,image4 FROM milestones
+   where (LOWER(title) LIKE $1) and timeline_id= $2;`, [`%${searchText}%`,timeline_id])
+    .then(({ rows: users }) => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Server Error');
+    });
+});
+
+
 module.exports = router;
