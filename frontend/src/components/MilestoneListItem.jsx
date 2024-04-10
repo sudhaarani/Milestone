e67fe864e6117bd1ad5fc7d350ff87d974dfe2cf
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-09 19:13:25 -0700

    format date and display title and date of the milestone

---
+++ frontend/src/components/MilestoneListItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/TimelineCard.css'
+//import FavButton from "./FavButton";
+
+const MilestoneListItem = ({ milestoneList}) => {
+  const { milestone_id, milestone_title, milestone_date  } = milestoneList;
+  const isoDate = new Date(milestone_date).toISOString();
+  const formattedDate = isoDate.substring(0, 10); // Extracting YYYY-MM-DD
+  console.log("formattedDate::",formattedDate);
+  return (
+    <div className='card'> 
+      <div className='card-body'>
+        <p className='card-title'>{milestone_title}</p>
+        <p className='card-text'>{formattedDate}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MilestoneListItem;
+
