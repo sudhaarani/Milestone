fd8c2357b85dadbe4f1788ce591c54c11c380ecb
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-09 19:09:24 -0700

    add getMilestonesByTimeline function to OnClick event of image tag, comment onClick event in edit icon

--- frontend/src/components/TimelineCard.jsx
+++ frontend/src/components/TimelineCard.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import '../styles/TimelineCard.css'
 import FavButton from "./FavButton";
 
-const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, handleFavourites, state }) => {
+const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, handleFavourites, state,getMilestonesByTimeline }) => {
   const { id, username, title, description, timelineImageUrl } = timelineList;
 
   return (
     <div className='card'>
       
-      <img src={timelineImageUrl} className='card-img-top' alt={title} />
+      <img src={timelineImageUrl} className='card-img-top' alt={title} onClick={() => { 
+            handleToggle() 
+            handleSelectedTimeline(id)
+            getMilestonesByTimeline(id)
+          }} />
       <div className='card-body'>
         <h5 className='card-title'>{title}</h5>
         <p className='card-text'>{description}</p>
@@ -16,10 +20,10 @@ const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, hand
       <div className='card-footer'>
         <p className='card-username'>By {username}</p>
         <i className='fa-solid fa-pen' 
-          onClick={() => { 
-            handleToggle() 
-            handleSelectedTimeline(id)
-          }} 
+          // onClick={() => { 
+          //   handleToggle() 
+          //   handleSelectedTimeline(id)
+          // }} 
         />
         <i className='fa-solid fa-trash'></i>
         <FavButton id={id} handleFavourites={handleFavourites} state={state} />
