da5d2d8c8d6546e2f74d7e6744f6e6f1a4b275ce
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-09 19:17:01 -0700

    add input tag

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBar = ({ keyword, onChange }) => {
+  const BarStyle = {width:"20rem",background:"#F0F0F0", border:"none", padding:"0.5rem"};
+  return (
+    <input 
+     style={BarStyle}
+     key="search-bar"
+     value={keyword}
+     placeholder={"search bar"}
+     onChange={(e) => onChange(e.target.value)}
+    />
+  );
+}
+
+export default SearchBar;
