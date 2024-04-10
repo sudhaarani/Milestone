635f6b85f25782c464acc3864db58b1c34103910
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-10 16:59:43 -0700

    add getClickedMilestone, handleMilestoneClicked in OnClick event

--- frontend/src/components/MilestoneListItem.jsx
+++ frontend/src/components/MilestoneListItem.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../styles/TimelineCard.css'
 //import FavButton from "./FavButton";
 
-const MilestoneListItem = ({ milestoneList}) => {
+const MilestoneListItem = ({ milestoneList,getClickedMilestone,handleMilestoneClicked}) => {
   const { milestone_id, milestone_title, milestone_date  } = milestoneList;
   const isoDate = new Date(milestone_date).toISOString();
   const formattedDate = isoDate.substring(0, 10); // Extracting YYYY-MM-DD
   // console.log("formattedDate::",formattedDate);
   return (
-    <div className='card'> 
+    <div className='card' onClick={() => {
+      getClickedMilestone(milestone_id)
+      handleMilestoneClicked()
+    }}> 
       <div className='card-body'>
         <p className='card-title'>{milestone_title}</p>
         <p className='card-text'>{formattedDate}</p>
