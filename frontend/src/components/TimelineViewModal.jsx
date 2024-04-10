c002a88f61665fb85ae8f97925d8ed18935050d4
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-10 01:17:43 -0700

    send setKeyword as props to SearchBar component and rename updateKeyword to searchkeyword

--- frontend/src/components/TimelineViewModal.jsx
+++ frontend/src/components/TimelineViewModal.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import '../styles/TimelineViewModal.css';
 import closeSymbol from '../assets/closeSymbol.svg';
 import MilestoneList from '../components/MilestoneList';
 import SearchBar from '../components/SearchBar';
 
-const TimelineViewModal = ({ handleToggle, state }) => {
+const TimelineViewModal = ({ handleToggle, state ,searchKeyword}) => {
   const { selectedTimeline } = state;
   const [keyword, setKeyword] = useState('');
-  // console.log({ selectedTimeline })
+// console.log({ selectedTimeline })
   // console.log(state.milestonesByTimeline)
   return (
     <div className='timeline-edit-modal'>
       <button className="close-button" onClick={() => { handleToggle() }}>
         <img src={closeSymbol} alt='close symbol' />
       </button>
-      <SearchBar keyword={keyword} onChange={updateKeyword} />
+      <SearchBar keyword={keyword} searchKeyword={searchKeyword} setKeyword={setKeyword}  />
       {selectedTimeline &&
         (<div>
             {/* < PhotoFavButton id={selectedPhoto.id} favPhotos={state.favPhotos} favPhotosClick={favPhotosClick} /> */}
