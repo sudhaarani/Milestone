0ddaa68ee0eb33a6e61d7bd276cd3684447f326f
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-10 16:57:00 -0700

    add MilestoneViewModal , isMilestoneClicked state and handle func for that state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import React from 'react';
+import React,{useState} from 'react';
 import HomePage from './components/HomePage';
 import MainModal from './components/MainModal';
 import TimelineViewModal from './components/TimelineViewModal';
+
+import MilestoneViewModal from './components/MilestoneViewModal';
 import NavBar from './components/NavBar';
 import NewTimelineForm from './components/NewTimelineForm';
 
@@ -11,8 +13,12 @@ import useToggle from "../src/hooks/useToggle";
 
 
 function App() {
-  const { state, handleSelectedTimeline, handleFavourites,getMilestonesByTimeline,searchKeyword } = useApplicationData();
-  const { toggleState, handleToggle} = useToggle();
+  const { state, handleSelectedTimeline, handleFavourites,getMilestonesByTimeline,searchKeyword,getClickedMilestone } = useApplicationData();
+  const { toggleState, handleToggle } = useToggle();
+  const [isMilestoneClicked, setIsMilestoneClicked] = useState(false);
+  const handleMilestoneClicked= () => {
+    setIsMilestoneClicked(!isMilestoneClicked);  
+  };
 
   return (
     <div className="App">
@@ -21,8 +27,10 @@ function App() {
       <HomePage state={state} handleToggle={handleToggle} handleSelectedTimeline={handleSelectedTimeline}
         handleFavourites={handleFavourites} getMilestonesByTimeline={getMilestonesByTimeline} />
 
-      {toggleState && <TimelineViewModal handleToggle={handleToggle} state={state} searchKeyword={searchKeyword} />}
+      {toggleState && <TimelineViewModal handleToggle={handleToggle} state={state} searchKeyword={searchKeyword}
+        getClickedMilestone={getClickedMilestone} handleMilestoneClicked={handleMilestoneClicked} />}
       {/* {toggleState && <MainModal handleToggle={handleToggle} state={state} />} */}
+      {isMilestoneClicked && <MilestoneViewModal handleMilestoneClicked={handleMilestoneClicked} state={state} />}
       <NewTimelineForm />
     </div>
   );
