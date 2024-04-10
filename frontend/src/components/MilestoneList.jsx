52a5feae3e8601ec84bfc7ba25a342fd1de841b9
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-10 16:58:56 -0700

    add getClickedMilestone, handleMilestoneClicked as porps to child component

--- frontend/src/components/MilestoneList.jsx
+++ frontend/src/components/MilestoneList.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import MilestoneListItem from './MilestoneListItem';
 //import '../styles/ListOfTimelines.css'
 
-const MilestoneList = ({ state }) => {
+const MilestoneList = ({ state,handleMilestoneClicked,getClickedMilestone }) => {
   const MilestoneListArray = state.milestonesByTimeline.map((milestoneList) => {
     return (<MilestoneListItem
               key={milestoneList.milestone_id}
               milestoneList={milestoneList}
+              getClickedMilestone={getClickedMilestone}
+              handleMilestoneClicked={handleMilestoneClicked}
             />)
   });
 
