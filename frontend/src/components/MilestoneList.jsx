d7e40bc64e6f73da2e3485c3445ab205e1e18159
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-11 16:25:01 -0700

    send isTimelineEditClicked props to child component

--- frontend/src/components/MilestoneList.jsx
+++ frontend/src/components/MilestoneList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import MilestoneListItem from './MilestoneListItem';
 //import '../styles/ListOfTimelines.css'
 
-const MilestoneList = ({ state,handleMilestoneClicked,getClickedMilestone }) => {
+const MilestoneList = ({ state,handleMilestoneClicked,getClickedMilestone,isTimelineEditClicked }) => {
   const MilestoneListArray = state.milestonesByTimeline.map((milestoneList) => {
     return (<MilestoneListItem
               key={milestoneList.milestone_id}
               milestoneList={milestoneList}
               getClickedMilestone={getClickedMilestone}
-              handleMilestoneClicked={handleMilestoneClicked}
+      handleMilestoneClicked={handleMilestoneClicked}
+      isTimelineEditClicked={isTimelineEditClicked}
             />)
   });
 
