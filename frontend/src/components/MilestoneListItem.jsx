c81cdc2c6de994d3ca6905b28f7866c66b040d99
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-11 20:00:06 -0700

    add getClickedMilestone in edit's onClick event

--- frontend/src/components/MilestoneListItem.jsx
+++ frontend/src/components/MilestoneListItem.jsx
@@ -3,24 +3,29 @@ import '../styles/TimelineCard.css'
 //import FavButton from "./FavButton";
 
 const MilestoneListItem = ({ milestoneList, getClickedMilestone, handleMilestoneClicked,
-  isTimelineEditClicked}) => {
+  isTimelineEditClicked,handleMilestoneEditClicked}) => {
   const { milestone_id, milestone_title, milestone_date  } = milestoneList;
   const isoDate = new Date(milestone_date).toISOString();
   const formattedDate = isoDate.substring(0, 10); // Extracting YYYY-MM-DD
   // console.log("formattedDate::",formattedDate);
   return (
-    <div className='card' onClick={() => {
-      getClickedMilestone(milestone_id)
-      handleMilestoneClicked()
-      }}> 
-      <div className='card-body'>
-        <p className='card-title'>{milestone_title}</p>
-        <p className='card-text'>{formattedDate}</p>
+    <>
+      <div className='card' onClick={() => {
+        getClickedMilestone(milestone_id)
+        handleMilestoneClicked()
+        }}> 
+        <div className='card-body'>
+          <p className='card-title'>{milestone_title}</p>
+          <p className='card-text'>{formattedDate}</p>
         </div>
-        {isTimelineEditClicked && <div>
-        <button>Edit</button><button>Delete</button>
+      </div>
+      {isTimelineEditClicked && <div>
+        <button onClick={() => { 
+          handleMilestoneEditClicked() 
+          getClickedMilestone(milestone_id)
+          }}>Edit</button><button>Delete</button>
       </div>}
-    </div>
+    </>
   );
 }
 
