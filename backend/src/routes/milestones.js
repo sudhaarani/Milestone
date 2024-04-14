fca67b48e0d4b5b191ea4bc25146f05c631caaab
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-13 21:39:04 -0700

    remove if statement above update query

--- backend/src/routes/milestones.js
+++ backend/src/routes/milestones.js
@@ -45,7 +45,6 @@ router.get('/milestones', (req, res) => {
 router.get('/milestones/search/:timeline_id/:searchText', (req, res) => {  
   let searchText = req.params.searchText;
   let timeline_id = req.params.timeline_id;
-  console.log("searchText:",searchText)
   searchText=searchText.toLowerCase();
   db.query(`SELECT id as milestone_id,timeline_id, title as milestone_title, date as milestone_date, image1,image2,image3,image4 FROM milestones
    where (LOWER(title) LIKE $1) and timeline_id= $2;`, [`%${searchText}%`,timeline_id])
@@ -61,36 +60,11 @@ router.get('/milestones/search/:timeline_id/:searchText', (req, res) => {
 //for milestone-edit save form
 //-------have to  implement deleting the previous image in the upload folder
 router.post('/milestones/update', upload.array('images', 4), (req, res) => {
-  const { title } = req.body;
-  const { date } = req.body;
-  const { diary_entry } = req.body;
-  const { images } = req.body;
-
-  const { milestone_id } = req.body;
-  const { timeline_id } = req.body;
-  let imagesArray = [];
-  if (Array.isArray(images)) {
-    // Handle array of files
-    for (let i = 0; i < images.length; i++) {
-      imagesArray.push(images[i]);
-    }
-  } else {
-    // If images is a string, it means only one file is uploaded
-    imagesArray.push(images);// = req.body.images;
-    // Process single image
-  }
-  console.log("save: timeline id:", timeline_id);
-  console.log("save: milestone_id :", milestone_id);
-  console.log("save: title :", title);
-  console.log("save: date:", date);
-  console.log("save: diary_entry:", diary_entry);
-  console.log("save: images:", images);
-  console.log("save: imagesArray:", imagesArray);
+  const { title,date,diary_entry,images,milestone_id,timeline_id } = req.body;
 
   db.query(`UPDATE milestones SET title =$1 , date=$2,diary_entry=$3, image1=$4,image2=$5,image3=$6,image4=$7
-  WHERE id = $8 and timeline_id=$9 RETURNING *;`, [title, date, diary_entry, imagesArray[0],imagesArray[1],imagesArray[2],imagesArray[3],milestone_id ,timeline_id])
+  WHERE id = $8 and timeline_id=$9 RETURNING *;`, [title, date, diary_entry, images[0],images[1],images[2],images[3],milestone_id ,timeline_id])
     .then(({ rows: milestones }) => {
-      
       res.json(milestones);
     })
     .catch((error) => {
