905a6807710f1f85ce6501c8b12fa9efaa1db76e
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-13 21:27:46 -0700

    remove handleMultiEditImageInput

--- frontend/src/hooks/useImageInput.js
+++ frontend/src/hooks/useImageInput.js
@@ -8,31 +8,13 @@ const useImageInput = (initialValue) => {
   };
 
   const handleMultiImageInput = (event) => {
-    console.log("event.target.files:", event.target.files);
     setImageInput(event.target.files);
-  };
-
-
-  const handleMultiEditImageInput = (images,event) => {
-    const uploadedFiles = event.target.files;
-    const updatedImages = [];
-  
-    // Loop through the total number of images you want to keep unchanged
-    for (let i = 0; i < 3; i++) {
-      console.log("images in event target:",images[i])
-      // If there's an uploaded file for this index, use it; otherwise, keep the existing image
-      updatedImages.push(uploadedFiles[i] || images[i]);
-    }
-  
-    // Update the state with the updated image array
-    setImageInput(updatedImages);
   };
 
   return {
     imageInput,
     handleImageInput,
     handleMultiImageInput,
-    handleMultiEditImageInput,
     reset: () => setImageInput(initialValue)
   };
 };
