3d6f35ce19d647cecc7e8700b6fd5495b82f176d
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-13 21:26:53 -0700

    done with editing 4 or 1, keeping the remaining images

--- frontend/src/components/MilestoneEditModal.jsx
+++ frontend/src/components/MilestoneEditModal.jsx
@@ -1,21 +1,30 @@
 
-import React,{useState,useEffect} from 'react';
+import React,{useState} from 'react';
 
 import '../styles/MilestoneEditModal.css';
 import closeSymbol from '../assets/closeSymbol.svg';
-import useImageInput from '../hooks/useImageInput';
 import useTextInput from '../hooks/useTextInput';
 
 const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
   const { selectedMilestone } = state;
-  console.log("selectedMilestone:", selectedMilestone);
+  //console.log("selectedMilestone:", selectedMilestone);
   const isoDate = new Date(selectedMilestone.milestone_date).toISOString();
   const formattedDate = isoDate.substring(0, 10); // Extracting YYYY-MM-DD
  
   const title = useTextInput(selectedMilestone.milestone_title);
   const date = useTextInput(formattedDate);
   const diary_entry = useTextInput(selectedMilestone.diary_entry);
-  const images = useImageInput([selectedMilestone.image1,selectedMilestone.image2,selectedMilestone.image3,selectedMilestone.image4]);
+  
+  const [imageInput, setImageInput] = useState([selectedMilestone.image1,selectedMilestone.image2,selectedMilestone.image3,selectedMilestone.image4]);
+  const handleMultiEditImageInput = (event) => {
+    const uploadedFiles = event.target.files;
+    const updatedImages = [];
+    for (let i = 0; i < 4; i++) {
+      // If there's an uploaded file for this index, use it; otherwise, keep the existing image
+      updatedImages.push(uploadedFiles[i]|| imageInput[i]);
+    }
+    setImageInput(updatedImages);
+  };
 
   //to close the modal once save btn is clicked and form has submitted(form has to be
   //submitted before it closes so delaying one sec)
@@ -31,19 +40,11 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
     formData.append('title', title.textInput);  
     formData.append('date', date.textInput);
     formData.append('diary_entry', diary_entry.textInput);
-    for (let i = 0; i < images.imageInput.length; i++) {
-      console.log("images.imageInput i:", images.imageInput[i]);
-      console.log("images.imageInput i:", images.imageInput[i].name);
-      formData.append(`images`, images.imageInput[i].name || images.imageInput[i]);
+    for (let i = 0; i < imageInput.length; i++) {
+      formData.append(`images`, imageInput[i].name || imageInput[i]); //only uploaded files have name(obj),for remaining imageInput[i], its string
     }
-    console.log("images.imageInput:", images.imageInput);
-    console.log("milestone_id:", selectedMilestone.milestone_id);
-    console.log("timeline.id:", selectedMilestone.id);
-    console.log("images:", selectedMilestone.milestoneImageUrl);
-    //console.log("images:", selectedMilestone.milestoneImageUrl);
     formData.append('milestone_id', selectedMilestone.milestone_id);
     formData.append('timeline_id', selectedMilestone.id);//this is timeline's id
-    //throws error when we save without editing image ---> have to look
 
     fetch('/api/milestones/update', {
       method: 'POST',
@@ -60,7 +61,6 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
       console.error('Error fetching data:', error);
     });
   }
-  // console.log(state.milestonesByTimeline)
   return (
     <div className='milestone-edit-modal'>
       <button className="close-button" onClick={() => { milestoneEditToggle.handleToggle() }} >
@@ -89,9 +89,7 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
               <img src={selectedMilestone.milestoneImageUrl[2]} className='card-img-top' alt={selectedMilestone.image3} />
               <img src={selectedMilestone.milestoneImageUrl[3]} className='card-img-top' alt={selectedMilestone.image4} />
               <div>
-              <input type="file" name="images" id="images" onChange={() => {
-                images.handleMultiEditImageInput(images.imageInput)
-              } } multiple />
+              <input type="file" name="images" id="images" onChange={handleMultiEditImageInput} multiple />
               </div>
             </div>
             <button type="submit" onClick={() => { handleSaveClose() }}>Save</button>
