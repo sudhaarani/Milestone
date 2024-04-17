a151b491d6e2adb3396bc146d9f8cbaf0c7e18c0
Rico Chow <rico.ck.chow@gmail.com>
2024-04-17 14:43:21 -0400

    Will show timelines associated with the username clicked on a timeline card

--- frontend/src/components/TimelineCard.jsx
+++ frontend/src/components/TimelineCard.jsx
@@ -1,8 +1,41 @@
+// import React from 'react';
+// import '../styles/TimelineCard.css'
+// import FavButton from "./FavButton";
+
+// const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, handleFavourites, state }) => {
+//   const { id, username, title, description, timelineImageUrl } = timelineList;
+
+//   return (
+//     <div className='card'>
+      
+//       <img src={timelineImageUrl} className='card-img-top' alt={title} />
+//       <div className='card-body'>
+//         <h5 className='card-title'>{title}</h5>
+//         <p className='card-text'>{description}</p>
+//       </div>
+//       <div className='card-footer'>
+//         <p className='card-username'>By {username}</p>
+//         <i className='fa-solid fa-pen' 
+//           onClick={() => { 
+//             handleToggle() 
+//             handleSelectedTimeline(id)
+//           }} 
+//         />
+//         <i className='fa-solid fa-trash'></i>
+//         <FavButton id={id} handleFavourites={handleFavourites} state={state} />
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default TimelineCard;
+
 import React from 'react';
 import '../styles/TimelineCard.css'
 import FavButton from "./FavButton";
 
-const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, handleFavourites, state }) => {
+const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, handleFavourites, handleUsernameClick, state }) => {
   const { id, username, title, description, timelineImageUrl } = timelineList;
 
   return (
@@ -14,7 +47,7 @@ const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, hand
         <p className='card-text'>{description}</p>
       </div>
       <div className='card-footer'>
-        <p className='card-username'>By {username}</p>
+      <a href="#" className='card-username' onClick={(event) => handleUsernameClick(username, event)}>By {username}</a>
         <i className='fa-solid fa-pen' 
           onClick={() => { 
             handleToggle() 
@@ -24,10 +57,8 @@ const TimelineCard = ({ timelineList, handleToggle, handleSelectedTimeline, hand
         <i className='fa-solid fa-trash'></i>
         <FavButton id={id} handleFavourites={handleFavourites} state={state} />
       </div>
-
     </div>
   );
 }
 
 export default TimelineCard;
-
