30cbe549348f4a472e29470f4caee6ca41846adc
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-04-18 16:53:47 -0700

    add edit button for each image, add button for remaining images and handle functions accordingly

--- frontend/src/components/MilestoneEditModal.jsx
+++ frontend/src/components/MilestoneEditModal.jsx
@@ -1,5 +1,6 @@
 
-import React,{useState,useEffect} from 'react';
+//if 2 available, displays image and edit button, add 2 more pen
+import React, { useState, useEffect } from 'react';
 
 import '../styles/MilestoneEditModal.css';
 import closeSymbol from '../assets/closeSymbol.svg';
@@ -7,46 +8,85 @@ import useTextInput from '../hooks/useTextInput';
 import useImageInput from '../hooks/useImageInput';
 const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
   const { selectedMilestone } = state;
-  //console.log("selectedMilestone:", selectedMilestone);
+  console.log("selectedMilestone:", selectedMilestone);
   const isoDate = new Date(selectedMilestone.milestone_date).toISOString();
   const formattedDate = isoDate.substring(0, 10); // Extracting YYYY-MM-DD
- 
-  // const title = useTextInput(selectedMilestone.milestone_title);
-  // const date = useTextInput(formattedDate);
-  // const diary_entry = useTextInput(selectedMilestone.diary_entry);
-   const images = useImageInput(null);
-
-  const [oldValues, setOldValues] = useState({
-    title: '',
-    date: '',
-    diary_entry: ''
-  });
-  const [editedValues, setEditedValues] = useState({
-    title: '',
-    date: '',
-    diary_entry: ''
-  });
-
-  // Simulated function to fetch old values from the database
-  const fetchOldValues = () => {
-    // Replace this with actual API call to fetch old values
-    const oldValuesFromDatabase = {
-      title: selectedMilestone.milestone_title,
-      date: formattedDate,
-      diary_entry: selectedMilestone.diary_entry
-    };
-    setOldValues(oldValuesFromDatabase);
-    setEditedValues(oldValuesFromDatabase); // Populate edited values with old values initially
+  const imagesNotNullInDbCount = selectedMilestone.milestoneImageUrl.length; 
+  console.log("imagesNotNullInDbCount:", imagesNotNullInDbCount);
+  
+  const oldValuesFromDatabase = {
+    title: selectedMilestone.milestone_title,
+    date: formattedDate,
+    diary_entry: selectedMilestone.diary_entry,
+    image1:selectedMilestone.milestoneImageUrl[0],
+    image2:selectedMilestone.milestoneImageUrl[1],
+    image3:selectedMilestone.milestoneImageUrl[2],
+    image4:selectedMilestone.milestoneImageUrl[3],
   };
-
-  useEffect(() => {
-    fetchOldValues(); // Fetch old values when the component mounts
-  }, []);
-
+  const [oldValues, setOldValues] = useState(oldValuesFromDatabase);
+  const [editedValues, setEditedValues] = useState(oldValuesFromDatabase);
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setEditedValues({ ...editedValues, [name]: value });
   };
+
+  const handleImageChange = (e) => {
+    const { name, files } = e.target;
+    if (name === 'image1' || 'image2' || 'image3' || 'image4') {
+      const imageFile = files[0];
+      setEditedValues({ ...editedValues, [name]: imageFile });
+      console.log("imageFile:", imageFile);
+    }
+    console.log("e.target:", e.target);
+    if (name === 'images') {
+      console.log("inside images(multiple) e.target.files:", files);
+      const lenOfTargetFiles = files.length;
+      console.log("lenOfTargetFiles:", lenOfTargetFiles);
+      if (lenOfTargetFiles === 4) {
+        setEditedValues({
+          ...editedValues, image1: files[0], image2: files[1],
+          image3: files[2], image4:files[3]
+        });
+      }
+      if (lenOfTargetFiles === 3) {
+        setEditedValues({
+          ...editedValues, image2: files[0],
+          image3: files[1], image4: files[2]
+        });
+      }
+      if (lenOfTargetFiles === 2) {
+        setEditedValues({
+          ...editedValues, image3: files[0], image4: files[1]
+        });
+      }
+      if (lenOfTargetFiles === 1) {
+        setEditedValues({
+          ...editedValues, image4: files[0]
+        });
+      }
+    }
+  };
+    // if (imageFile) {
+    //   // File/Blob object is present
+    //   const reader = new FileReader();
+    //   reader.onloadend = () => {
+    //       setEditedValues({ ...editedValues, [name]: reader.result });
+    //   }
+    //   reader.readAsDataURL(imageFile);
+    //   console.log("reader.result :", reader.result );
+    // }else {
+    // setEditedValues({ ...editedValues, [name]: imageFile });
+    // }
+    // const reader = new FileReader();
+    
+    // reader.onloadend = () => {
+    //   setEditedValues({ ...editedValues, [name]: reader.result });
+    // }
+    // if (firstFile) {
+    //   reader.readAsDataURL(firstFile);
+    // }
+  
 
   //to close the modal once save btn is clicked and form has submitted(form has to be
   //submitted before it closes so delaying one sec)
@@ -59,20 +99,17 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
   const handleTimelineSave = (event) => {
     event.preventDefault();
     const formData = new FormData();
-    // console.log("title.textInput:", title.textInput);
-    // console.log("date.textInput:", date.textInput);
-    
-    formData.append('title', oldValues.title === editedValues.title ? '' :editedValues.title);  
-    formData.append('date', oldValues.date === editedValues.date ? '' :editedValues.date);
-    formData.append('diary_entry', oldValues.diary_entry === editedValues.diary_entry ? '' :editedValues.diary_entry);
-    if(images.imageInput){
-      for (let i = 0; i < images.imageInput.length; i++) {
-        formData.append(`images`, images.imageInput[i]);
+    console.log("editedValues:", editedValues);
+    console.log("oldValues:", oldValues);
+
+    Object.keys(editedValues).forEach(fieldName => {
+      const file = oldValues[fieldName] === editedValues[fieldName] ? '' :editedValues[fieldName];
+      if (file) {
+        formData.append(fieldName, file);
       }
-    }
+    });
     formData.append('milestone_id', selectedMilestone.milestone_id);
     formData.append('timeline_id', selectedMilestone.id);//this is timeline's id
-    //formData.append('imageCount', selectedMilestone.milestoneImageUrl.length);
     
     fetch('/api/milestones/update', {
       method: 'POST',
@@ -94,10 +131,9 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
       <button className="close-button" onClick={() => { milestoneEditToggle.handleToggle() }} >
         <img src={closeSymbol} alt='close symbol' />
       </button>
-      
       {selectedMilestone &&
         (<div>
-          <form onSubmit={handleTimelineSave} encType="multipart/form-data">
+          <form onSubmit={handleTimelineSave} enctype="multipart/form-data">
             <div>
               <label>Title:</label>
               <input type="text" name="title" id="title" value={editedValues.title} onChange={handleChange} />
@@ -112,23 +148,30 @@ const MilestoneEditModal = ({ state,milestoneEditToggle }) => {
             </div>
             <div>
               <label>Images:</label>
-              {selectedMilestone.image1 &&
-                <img src={selectedMilestone.milestoneImageUrl[0]} className='card-img-top' alt={selectedMilestone.image1} />}
-              {selectedMilestone.image2 &&
-                <img src={selectedMilestone.milestoneImageUrl[1]} className='card-img-top' alt={selectedMilestone.image2} />}
-              {selectedMilestone.image3 &&
-                <img src={selectedMilestone.milestoneImageUrl[2]} className='card-img-top' alt={selectedMilestone.image3} />}
-              {selectedMilestone.image4 &&
-                <img src={selectedMilestone.milestoneImageUrl[3]} className='card-img-top' alt={selectedMilestone.image4} />}
+              {Object.entries(editedValues).slice(3, 7).map(([key, value], index) => (  
+                value &&  value !== '/uploads/null' &&
+                  (<div>
+                    <label for={key}>
+                      <i className="fa-solid fa-pen" />
+                    </label>
+                    <input type="file" name={key} id={key} onChange={handleImageChange} style={{ display: 'none' }} />
+                    <img src={value} className='card-img-top' alt={value.name} />
+                  </div>)
+                  ))}
               <div>
-                <label for="images" class="btn btn-primary">Change Image</label>
-                <input type="file" name="images" id="images" onChange={images.handleMultiImageInput} multiple style={{ display: 'none' }} />
+                <label for="images" class="btn btn-primary">
+                {imagesNotNullInDbCount === 0 ? 'Add (Max of 4)' :
+                  imagesNotNullInDbCount === 1 ? 'Add (Max of 3)' :
+                  imagesNotNullInDbCount === 2 ? 'Add (Max of 2)' :
+                  imagesNotNullInDbCount === 3 ? 'Add (Max of 1)' : ""}
+                </label>
+                <input type="file" name="images" id="images" onChange={handleImageChange} multiple style={{ display: 'none' }} />
               </div>
             </div>
             <button type="submit" onClick={() => { handleSaveClose() }}>Save</button>
           </form>
-        </div>)}
-      
+        </div>)
+      }  
     </div>
   )
 };
